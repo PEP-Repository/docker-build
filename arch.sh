b36534c48e6b5e6cc727f162d77e16d2c2d7ab83
Bernard van Gastel <bvgastel@bitpowder.com>
2021-03-14 09:20:20 +0100

    Removing arch, as it gives problems with kernels not in sync (which will be an issue in the future with Docker).

--- arch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-VERSION=`date +%Y.%m`.01
-wget -c http://mirror.nl.leaseweb.net/archlinux/iso/$VERSION/archlinux-bootstrap-$VERSION-x86_64.tar.gz
-sudo tar -xzpf archlinux-bootstrap-$VERSION-x86_64.tar.gz
-(cd root.x86_64/; sudo tar -cypf ../archlinux.tar.bz2 .)
-sudo rm -rf root.x86_64
-docker build --rm=true -f Dockerfile -t "bitpowder/archlinux" . && docker push bitpowder/archlinux
