8198acadfce4f64901ee8486592982f42a5fd382
Hans Harmannij <hans.harmannij@imec.nl>
2021-05-03 13:44:51 +0200

    Fix run-pipeline script

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -22,28 +22,27 @@ then
   exit 1
 fi
 
-# POSIX shell does not have arrays, so we're using strings. The quotes are there so we can easily check for the whole word, otherwise substrings would also match
 # All possible statuses are documented on https://docs.gitlab.com/ee/api/pipelines.html. I cannot fine any documentation on what these statuses mean.
 # Not all statuses are listed below. I don't expect we will encounter the missing statuses, but if we do we must investigate in which category they should fall.
-running_statuses="'pending' 'running' 'created' 'preparing' 'waiting_for_resource'"
-success_statuses="'success' 'skipped'"
-failure_statuses="'failed' 'canceled'"
+running_statuses="\"pending\" \"running\" \"created\" \"preparing\" \"waiting_for_resource\""
+success_statuses="\"success\" \"skipped\""
+failure_statuses="\"failed\" \"canceled\""
 
 
 status="\"pending\""
-while contains "$running_statuses" "'$status'"
+while true
 do
   status=$(curl -sS --header "PRIVATE-TOKEN:${GITLAB_ACCESS_TOKEN}" "https://gitlab.pep.cs.ru.nl/api/v4/projects/pep%2fcore/pipelines/${pipelineid}" | jq ".status")
 
-  if contains "$success_statuses" "'$status'"
+  if contains "$success_statuses" "$status"
   then
     echo "Pipeline succeeded with status: '$status'"
     exit 0
-  elif contains "$failure_statuses" "'$status'"
+  elif contains "$failure_statuses" "$status"
   then
     echo "Pipeline failed with status: '$status'"
     exit 1
-  elif not contains "$running_statuses" "'$status'"
+  elif ! contains "$running_statuses" "$status"
   then
     echo "Received unsupported status \"$status\" from Gitlab API"
     exit 1
