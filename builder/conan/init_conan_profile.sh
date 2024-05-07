f8849356a60722a44e4fccb4d0470aeb60c1cd60
Steven WdV <swdv@cs.ru.nl>
2024-05-07 12:26:52 +0200

    Save compiler executable in conan_profile and use that for creating a local profile via init_conan_profile.sh Update Conan README part

---
+++ builder/conan/init_conan_profile.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+# You can override the Conan profile name by passing it as the first parameter
+profile_name="${1:-default}"
+
+conan_home="$(conan config home)"
+conan_profile="$conan_home/profiles/$profile_name"
+
+if [ -e "$conan_profile" ]; then
+  read -rp "$conan_profile already exists, replace? (y/n): " choice
+  case $choice in
+    [Yy]* ) ;;
+    * )
+      echo Aborting
+      exit 1;;
+  esac
+fi
+
+# Render our autodetect profile and take one section ('Build profile:') to put in the profile file (and print)
+conan profile show --profile:all="${0%/*}/conan_profile" |
+  awk 'p==1 {print $0} /Build profile:/ {p=1}' |
+  tee "$conan_profile"
+echo "Profile written to $conan_profile"
