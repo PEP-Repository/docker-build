57cdee1b2649227f9ca434df55c05abe79662c5d
Sander Meis <smeis@science.ru.nl>
2024-08-30 10:10:38 +0200

    set openssldir for macos

--- builder/conan/conanfile.py
+++ builder/conan/conanfile.py
@@ -201,7 +201,6 @@ class PepRecipe(ConanFile):
             'no_rc2': True,
             'no_rc4': True,
             'no_ssl3': True,
-            **({'openssldir': '/etc/ssl'} if self.settings.os == 'Macos' else {}),
         }))
         self.requires('prometheus-cpp/[^1.1]', options=self._optional_opts({
             'with_pull': False,
