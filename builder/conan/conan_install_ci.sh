362e48d1342b3ab3d5770d61b2bda4dfed421933
Steven WdV <swdv@cs.ru.nl>
2025-06-17 13:01:19 +0200

    Add weblib CI build

--- builder/conan/conan_install_ci.sh
+++ builder/conan/conan_install_ci.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env sh
 
 set -eu
-concurrency_limit=${1:-}
 
 # cd to script directory
 cd "$(dirname -- "$0")"
-
-if [ -n "$concurrency_limit" ]; then
-  concurrency_option_conan="-c:a tools.build:jobs=$concurrency_limit"
-else
-  concurrency_option_conan=''
-fi
 
 mkdir -p "$(conan config home)/profiles/"
 cp ./conan_profile "$(conan config home)/profiles/default"
@@ -21,19 +14,17 @@ echo '==== Installing Release packages ===='
 conan install ./ --build=missing \
   --lockfile=./conan-ci.lock \
   -s build_type=Release \
-  $concurrency_option_conan \
-  -o "&:with_assessor=False" \
   -o "&:with_tests=True" \
-  -o "&:with_benchmark=True"
+  -o "&:with_benchmark=True" \
+  "$@"
 
 echo '==== Installing Debug packages ===='
 conan install ./ --build=missing \
   --lockfile=./conan-ci.lock \
   -s build_type=Debug \
-  $concurrency_option_conan \
-  -o "&:with_assessor=False" \
   -o "&:with_tests=True" \
-  -o "&:with_benchmark=False"
+  -o "&:with_benchmark=False" \
+  "$@"
 
 echo 'Cleaning cache'
 # Remove old packages
---
+++ builder/weblib.Dockerfile
@@ -0,0 +1,16 @@
+FROM ${BASE_IMAGE}
+ENV DEBIAN_FRONTEND=noninteractive
+
+COPY ./weblib.apt /tmp/
+
+# should be in one RUN command, to avoid huge caches between steps
+RUN apt-get update && \
+    apt-get install -y $(cat /tmp/weblib.apt | cut -d'#' -f1) && \
+    apt-get clean && rm -rf /var/cache/* /var/lib/{apt,dpkg,cache,log}/* /tmp/* /var/tmp/*
+
+RUN git clone --depth=1 https://github.com/emscripten-core/emsdk.git && \
+    /emsdk/emsdk install latest && \
+    /emsdk/emsdk activate latest && \
+    (cd /emsdk/upstream/emscripten/ && npm install)
+
+ENV DEBIAN_FRONTEND=''
